ea73e1baf9fa2787afb200d5e242903cc52851a0
Navpreet kaur <navmashiana60@gmail.com>
2022-03-24 15:13:00 -0400

    made the website responsive and minified css, js files

--- view/header.php
+++ view/header.php
@@ -12,8 +12,8 @@
 </head>
 <body>
     <header class="header" id="navbar-collapse-toggle">
-        <ul class="icon-menu">
-            <li class="icon-box">
+        <ul class="icon-menu d-none d-lg-block revealator-slideup revealator-once revealator-delay1">
+            <li class="icon-box active">
                 <i class="fa fa-home"></i>
                 <a href="index.php">
                     <h2>Home</h2>
@@ -38,5 +38,22 @@
                 </a>
             </li>
         </ul>
+
+        <!-- Mobile Menu Starts -->
+    <nav role="navigation" class="d-block d-lg-none">
+        <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul class="list-unstyled" id="menu">
+                <li class="active"><a href="index.php"><i class="fa fa-home"></i><span>Home</span></a></li>
+                <li><a href="about.php"><i class="fa fa-user"></i><span>About</span></a></li>
+                <li><a href="portfolio.php"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li>
+                <li><a href="contact.php"><i class="fa fa-envelope-open"></i><span>Contact</span></a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Mobile Menu Ends -->
     </header>
    
