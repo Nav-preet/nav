c7d1cd97626962c04dfa6bed92a6afa5c4d781f7
Navpreet kaur <navmashiana60@gmail.com>
2022-03-24 19:47:41 -0400

    added code to check error

--- contact_mail.php
+++ contact_mail.php
@@ -1,6 +1,7 @@
 <?php
 include('smtp/PHPMailerAutoload.php');
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $name = $_POST["name"];
 $email = $_POST["email"];
 $msg = $_POST["message"];
